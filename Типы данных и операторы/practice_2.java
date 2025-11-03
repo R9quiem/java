a73943c8d9ac8896f3f96d80ac93e721de3e697a
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-03 21:21:21 +0300

    Add practice_2.java

---
+++ Типы данных и операторы/practice_2.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "\u0415\u0444\u0438\u043C";
+        System.out.println(name); // Вывод: Ефим
+    }
+}
