07c347a5cea1d10de69574c7a80781e089f25b10
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-03 21:04:05 +0300

    Add practice_1.java

---
+++ Типы данных и операторы/practice_1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 0;
+        int i = -100000;
+        long l = 10000000000L;
+        float f = 3.14f;
+        double d = 3.14159;
+        char c = 'F';
+        boolean flag = true;
+    }
+}
