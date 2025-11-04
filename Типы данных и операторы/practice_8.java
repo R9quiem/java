a3bef0e42a7c837c6946bc6c6e3a7665957ad5d8
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-04 15:32:29 +0300

    Add practice_8.java

---
+++ Типы данных и операторы/practice_8.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var message = "Привет, Java!";
+        var count = 5;
+        var pi = 3.14;
+
+        System.out.println(message); // String
+        System.out.println(count);   // int
+        System.out.println(pi);      // double
+    }
+}
