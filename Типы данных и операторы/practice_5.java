e7211f468f8eea3ac25b82183ec6a1487019949c
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-04 15:08:56 +0300

    Add practice_5.java

---
+++ Типы данных и операторы/practice_5.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 5;
+        int b = 303003;
+        System.out.println("Пример int + byte -> int: " + (a+b));
+
+        int c = 100;
+        double d = 11.11;
+        System.out.println("Пример int + double -> double: " + (c+d));
+    }
+}
