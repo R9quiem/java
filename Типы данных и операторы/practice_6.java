f4266cf4e0ef4a64794a295413baddc720a1a756
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-04 15:14:24 +0300

    Add practice_6.java

---
+++ Типы данных и операторы/practice_6.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i1 = 120;
+        byte b = (byte)i1;
+        System.out.println(b);
+
+        float f = 100.1f;
+        int i2 = (byte)f;
+        System.out.println(i2);
+    }
+}
