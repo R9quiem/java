fdd512910c4cd4990f87414cc8dfcbffa56d41f2
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-04 15:49:58 +0300

    add practice_1
    Добавлено документирование класса Example и метода sum.

--- Типы данных и операторы/Документирование кода/practice_1.java
+++ Типы данных и операторы/Документирование кода/practice_1.java
@@ -1,1 +1,22 @@
+package org.example;
 
+/**
+ * Краткое описание в одном-двух предложениях.
+ * Более подробное описание при необходимости.
+ *
+ * @author  Ефим
+ * @since   1.0
+ */
+public class Example {
+    /**
+     * Возвращает сумму двух чисел.
+     *
+     * @param a первое слагаемое
+     * @param b второе слагаемое
+     * @return сумма a и b
+     * @throws IllegalArgumentException если a или b некорректны
+     */
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
