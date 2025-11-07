03490f4b11ce2cf51d465673c6f4f5662184f99e
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-08 02:41:55 +0300

    Update Main.java

--- Классы и методы/Ограничение доступа/practice_1/src/main/Main.java
+++ Классы и методы/Ограничение доступа/practice_1/src/main/Main.java
@@ -1,1 +1,24 @@
+package main;
+
+import util.A;
+
+class B extends A {
+    int e = 5;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show(); // работает всегда — метод public
+
+        System.out.println(obj.a); // ✅ public — видно
+        //System.out.println(obj.b); // ❌ protected — не видно без наследования
+        // System.out.println(obj.c); // ❌ package-private — другой пакет
+        // System.out.println(obj.d); // ❌ private — закрыт
+
+        B child = new B();
+        System.out.println(child.e); // ✅ package-private — видно в текущем пакете
+    }
+}
+
 
