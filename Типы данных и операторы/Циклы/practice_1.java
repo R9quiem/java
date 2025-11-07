a60fcc4cdb6e1c4bba02c26aa00e63d66d349b40
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-07 19:12:31 +0300

    Create practice_1.java

---
+++ Типы данных и операторы/Циклы/practice_1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        while (true) {
+            //do smth
+        }
+
+        do {
+            //do smth
+        } while(true);
+    }
+}
+
