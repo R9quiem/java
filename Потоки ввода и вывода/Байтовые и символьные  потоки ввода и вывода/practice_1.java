e9567404ec5d339ed244ee595ebb854e1c9121e0
R9quiem <139074792+R9quiem@users.noreply.github.com>
2025-11-20 18:36:05 +0300

    Update practice_1.java

--- Потоки ввода и вывода/Байтовые и символьные  потоки ввода и вывода/practice_1.java
+++ Потоки ввода и вывода/Байтовые и символьные  потоки ввода и вывода/practice_1.java
@@ -1,1 +1,27 @@
+package main;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        try (InputStream is = new FileInputStream("data.txt")) {
+
+            int b;
+            while ((b = is.read()) != -1) {   // читаем по одному байту
+                System.out.println("Прочитано: " + b +
+                        " символ: " + (char) b);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/*Если в файле data.txt например находится ABC, то выведет:
+Прочитано: 65 символ: A
+Прочитано: 66 символ: B
+Прочитано: 67 символ: C
+*/
